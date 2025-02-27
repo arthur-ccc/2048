470dd7b13ef47f76a7276a84604c160f316f65cf
Arthur Fernandes Vieira <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-02-27 12:06:22 -0300

    update Peca.hs, corrigindo retorno de geraPeca

--- haskell2048/app/Peca.hs
+++ haskell2048/app/Peca.hs
@@ -5,7 +5,7 @@ import System.Random (StdGen, Random(randomR))
 
 type Peca = (Int, Int, Int)
 
-geraPeca :: StdGen -> Int -> Int -> (Int, Int, Int) -- função que gera as posições aleatórias, a peça sai com o valor default de 2
+geraPeca :: StdGen -> Int -> Int -> Peca -- função que gera as posições aleatórias, a peça sai com o valor default de 2
 geraPeca gen i f =
     let (x, gen')  = randomR (i, f) gen
         (y, gen'') = randomR (i, f) gen'
